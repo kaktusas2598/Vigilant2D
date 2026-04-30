33109d7ffe3d4571e0862ec276f17efa474820ff
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2026-04-30 22:07:59 +0100

    Adding test assets for tiled map loading testing

---
+++ assets/Environment.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Environment" tilewidth="16" tileheight="16" tilecount="5760" columns="64">
+ <image source="Retro-Lines-16x16/Environment.png" width="1024" height="1440"/>
+</tileset>
